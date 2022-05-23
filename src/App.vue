8681c64cb364ce2a41e397241ab917c2cc7121ea
Taufan Basri <taufanprasetyobasri@gmail.com>
2022-05-23 17:32:15 +0700

    Scoped Slots

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+        <h2>
+          {{ slotProps.item }}
+        </h2>
+        <p>
+          {{ slotProps['another-prop'] }}
+        </p>
+    </course-goals>
   </div>
 </template>
 
@@ -14,10 +22,11 @@
 import BadgeList from './components/BadgeList.vue';
 import TheHeader from './components/TheHeader.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
-    BadgeList, TheHeader, UserInfo
+    BadgeList, TheHeader, UserInfo, CourseGoals
   },
   data() {
     return {
